e1b73648597eef34111ca8a83b5440cde85f12f2
Jim Yeh <jim@bitmark.com>
2017-06-19 09:32:12 +0800

    Use bytes as the type of an address header and a script header

--- coinparams.go
+++ coinparams.go
@@ -26,42 +26,34 @@ var (
 	//BitcoinMain is params for main net.
 	BitcoinMain = &address.Params{
 		DumpedPrivateKeyHeader: []byte{128},
-		AddressHeader:          0,
-		P2SHHeader:             5,
+		AddressHeader:          []byte{0},
+		P2SHHeader:             []byte{5},
 		HDPrivateKeyID:         []byte{0x04, 0x88, 0xad, 0xe4},
 		HDPublicKeyID:          []byte{0x04, 0x88, 0xb2, 0x1e},
 	}
 	//BitcoinTest is params for test net.
 	BitcoinTest = &address.Params{
 		DumpedPrivateKeyHeader: []byte{239},
-		AddressHeader:          111,
-		P2SHHeader:             196,
+		AddressHeader:          []byte{111},
+		P2SHHeader:             []byte{196},
 		HDPrivateKeyID:         []byte{0x04, 0x35, 0x83, 0x94},
 		HDPublicKeyID:          []byte{0x04, 0x35, 0x87, 0xcf},
 	}
 	//LitecoinMain is params for litecoin main net.
 	LitecoinMain = &address.Params{
 		DumpedPrivateKeyHeader: []byte{176},
-		AddressHeader:          48,
-		P2SHHeader:             50,
+		AddressHeader:          []byte{48},
+		P2SHHeader:             []byte{50},
 		HDPrivateKeyID:         []byte{0x04, 0x88, 0xad, 0xe4},
 		HDPublicKeyID:          []byte{0x04, 0x88, 0xb2, 0x1e},
 	}
 	//LitecoinTest is params for litecoin test net.
 	LitecoinTest = &address.Params{
 		DumpedPrivateKeyHeader: []byte{239},
-		AddressHeader:          111,
-		P2SHHeader:             196,
+		AddressHeader:          []byte{111},
+		P2SHHeader:             []byte{196},
 		HDPrivateKeyID:         []byte{0x04, 0x35, 0x83, 0x94},
 		HDPublicKeyID:          []byte{0x04, 0x35, 0x87, 0xcf},
-	}
-	//MonacoinMain is params for monacoin main net.
-	MonacoinMain = &address.Params{
-		DumpedPrivateKeyHeader: []byte{178, 176},
-		AddressHeader:          50,
-		P2SHHeader:             5,
-		HDPrivateKeyID:         []byte{0x04, 0x88, 0xad, 0xe4},
-		HDPublicKeyID:          []byte{0x04, 0x88, 0xb2, 0x1e},
 	}
 )
 
